3ed619843d2b32ec555d863200b61b6452009721
Moises P. Sena <moisespsena@gmail.com>
2019-06-13 15:30:41 -0300

    Fix error

--- error.go
+++ error.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"os"
 
+	"strings"
+
 	"github.com/go-errors/errors"
 )
 
 type PathError struct {
-	Name string
-	Err  error
+	Name     string
+	Err      error
+	Messages []string
+}
+
+func (this *PathError) AddMessage(msg ...string) *PathError {
+	this.Messages = append(this.Messages, msg...)
+	return this
 }
 
 var (
@@ -18,39 +26,52 @@ var (
 )
 
 func (ar *PathError) Error() string {
-	return fmt.Sprintf("Asset %q: %v", ar.Name, ar.Err)
+	return fmt.Sprintf("Asset %q: %v", ar.Name, strings.Join(append([]string{ar.Err.Error()}, ar.Messages...), " -- "))
 }
 
 func ErrNotFound(name string) error {
-	return &PathError{name, os.ErrNotExist}
+	return &PathError{Name: name, Err: os.ErrNotExist}
 }
 
 func ErrNotDir(name string) error {
-	return &PathError{name, errNotIsDir}
+	return &PathError{Name: name, Err: errNotIsDir}
 }
 
 func ErrNotFile(name string) error {
-	return &PathError{name, errNotIsFile}
+	return &PathError{Name: name, Err: errNotIsFile}
 }
 
-func IsErr(other, err error) bool {
-	if err == nil {
+func IsErr(this error, err ...error) bool {
+	if this == nil {
 		return false
 	}
-	if ae, ok := err.(*PathError); ok && ae.Err == other {
-		return true
+	for _, err := range err {
+		if err == nil {
+			continue
+		}
+		if this == err {
+			return true
+		}
+		if ae, ok := this.(*PathError); ok {
+			if ae.Err == err {
+				return true
+			}
+		}
+		if comp, ok := err.(interface{ Is(error) bool }); ok && comp.Is(err) {
+			return true
+		}
 	}
 	return false
 }
 
 func IsNotFound(err error) (ok bool) {
-	return IsErr(errNotIsFile, os.ErrNotExist)
+	return IsErr(err, errNotIsFile, os.ErrNotExist)
 }
 
 func IsNotDir(err error) (ok bool) {
-	return IsErr(errNotIsDir, err)
+	return IsErr(err, errNotIsDir)
 }
 
 func IsNotFile(err error) (ok bool) {
-	return IsErr(errNotIsFile, err)
+	return IsErr(err, errNotIsFile)
 }
